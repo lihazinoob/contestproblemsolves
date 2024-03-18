859c0e7ad3a423eb024747c68eda121c785bb079
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-03-18 12:12:18 +0600

    Codeforces Round 633 (Div. 2)

---
+++ A.Filling_diamonds.cpp
@@ -0,0 +1,17 @@
+//Problem Link : https://codeforces.com/problemset/problem/1339/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    long long int n;
+    cin>>n;
+    cout<<n<<endl;
+    
+  }
+  return 0;
+}
