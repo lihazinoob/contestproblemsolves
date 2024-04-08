96e97ade892d268b72b793a53e4986fbdbca413e
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-04-08 14:43:50 +0600

    Codeforces Round 935 (Div. 3)

---
+++ A.Setting_up_camp.cpp
@@ -0,0 +1,32 @@
+//Problem Link: https://codeforces.com/contest/1945/problem/A 
+
+
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+    ll single, poly, uni;
+    cin >> single >> poly >> uni;
+    ll needPoly = (3 - poly % 3) % 3;
+    if (poly > 0 && needPoly > uni) {
+        cout << "-1\n";
+        return;
+    }
+
+    uni -= needPoly;
+    poly += needPoly;
+
+    ll mn = single + uni / 3 + (uni % 3 + 1) / 2 + poly / 3;
+    cout << mn << '\n';
+}
+
+int32_t main() {
+    ll T;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
