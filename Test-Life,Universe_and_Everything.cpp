b11163892ac98b3130d2e69d3de8826de67b37a7
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-04-12 16:47:25 +0600

    SPOJ Loop Basic Problem

---
+++ Test-Life,Universe_and_Everything.cpp
@@ -0,0 +1,22 @@
+//Problem Link : https://www.spoj.com/problems/TEST/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  // vector<int>naveed;
+  while(true)
+  {
+    int n;
+    cin>>n;
+    if(n == 42)
+    {
+      break;
+    } 
+    cout<<n<<endl;
+  }
+
+
+  return 0;
+}
