4fcb25d3ec28c5c123c199956b7c49c132851ffd
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-04-16 07:00:06 +0600

    Codeforces Round 496 (Div. 3)

---
+++ A.Tanya_and_the_staircase.cpp
@@ -0,0 +1,27 @@
+// Problem Link: https://codeforces.com/contest/1005/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin >> n;
+  vector<int> a;
+  int p = -1;
+  for (int i = 0; i < n; i++)
+  {
+    int x;
+    cin >> x;
+    if (x == 1 && p != -1)
+    a.push_back(p);
+    p = x;
+  }
+  a.push_back(p);
+  cout << a.size() << endl;
+  for (int i : a)
+  {
+    cout << i << " ";
+  }
+
+  return 0;
+}
